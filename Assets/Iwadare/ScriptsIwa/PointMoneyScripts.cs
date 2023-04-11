3a739de104f015d241b247827187e73c4042e3e8
DRGO <abokado0901@gmail.com>
2023-04-11 20:02:11 +0900

    お金吸収機能実装

--- Assets/Iwadare/ScriptsIwa/PointMoneyScripts.cs
+++ Assets/Iwadare/ScriptsIwa/PointMoneyScripts.cs
@@ -6,8 +6,12 @@ public class PointMoneyScripts : MonoBehaviour
 {
     GameManager _gm;
     Rigidbody2D _rb;
+    GameObject _player;
     [SerializeField]int _point = 100;
     [SerializeField] float _speed = 2f;
+    [SerializeField] float _getSpeed = 5f;
+    bool _moneyget;
+    [SerializeField] float _y = -5.3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,22 @@ public class PointMoneyScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(_moneyget)
+        {
+            if(_player)
+            {
+                Vector3 dir = (_player.transform.position - transform.position) * _getSpeed;
+                transform.Translate(dir * Time.deltaTime);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        if(transform.position.y < _y)
+        {
+            Destroy(gameObject);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,5 +47,11 @@ public class PointMoneyScripts : MonoBehaviour
             _gm.AddScore(_point);
             Destroy(gameObject);
         }
+    }
+
+    public void MoneyGet(GameObject player)
+    {
+        _player = player;
+        _moneyget = true;
     }
 }
