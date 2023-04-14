038f362e77155c39cde67d95c19108f3c0cd8aa8
DRGO <abokado0901@gmail.com>
2023-04-14 21:08:36 +0900

    PowerUpに上限を追加

--- Assets/Iwadare/ScriptsIwa/PowerUp.cs
+++ Assets/Iwadare/ScriptsIwa/PowerUp.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class PowerUp : SingletonMonovihair<PowerUp>
 {
-    public int _bakuhatuPower = 1;
-    public int _speedUp = 1;
-    public int _airnum = 1;
+    [Range(1,50)]public int _bakuhatuPower = 1;
+    [Range(1,30)]public int _speedUp = 1;
+    [Range(1,8)]public int _airnum = 1;
     private GameManager GM;
     [SerializeField] Text _powerText;
     [SerializeField] Text _speedText;
@@ -27,25 +27,53 @@ public class PowerUp : SingletonMonovihair<PowerUp>
 
     public void AllTextShow()
     {
-        TextShow(_powerText, _bakuhatuPower);
-        TextShow(_speedText, _speedUp);
-        TextShow(_airNumText, _airnum);
-        TextShow(_powerMoneyText, _bakuhatuPower * 500);
-        TextShow(_speedMoneyText, _speedUp * 50);
-        TextShow(_airnumMoneyText, _airnum * 5000);
+        if (_bakuhatuPower < 50)
+        {
+            TextShow(_powerText, _bakuhatuPower);
+            TextShow(_powerMoneyText, _bakuhatuPower * 500);
+        }
+        else
+        {
+            TextShow(_powerText, _bakuhatuPower,true);
+            TextShow(_powerMoneyText, _bakuhatuPower * 500,true);
+        }
+        if (_speedUp < 30)
+        {
+            TextShow(_speedMoneyText, _speedUp * 50);
+            TextShow(_speedText, _speedUp);
+        }
+        else
+        {
+            TextShow(_speedText, _speedUp,true);
+            TextShow(_speedMoneyText, _speedUp * 50,true);
+        }
+        if (_airnum < 8)
+        {
+            TextShow(_airNumText, _airnum);
+            TextShow(_airnumMoneyText, _airnum * 5000);
+        }
+        else
+        {
+            TextShow(_airNumText, _airnum,true);
+            TextShow(_airnumMoneyText, _airnum * 5000,true);
+        }
     }
 
-    void TextShow(Text text, int up)
+    void TextShow(Text text, int up,bool max = false)
     {
-        if(text)
+        if(text && !max)
         {
             text.text = up.ToString();
+        }
+        else if(text)
+        {
+            text.text = "MAX!";
         }
     }
     /// <summary>爆発の威力をアップさせる関数</summary>
     public void BakuhatuUp()
     {
-        if (GM._totalMoney >= _bakuhatuPower * 500)
+        if (GM._totalMoney >= _bakuhatuPower * 500 && _bakuhatuPower < 50)
         {
             GM.TotalMoney(-_bakuhatuPower * 500);
             _bakuhatuPower++;
@@ -55,7 +83,7 @@ public class PowerUp : SingletonMonovihair<PowerUp>
     /// <summary>球のスピードをアップさせる関数</summary>
     public void SpeedUp()
     {
-        if (GM._totalMoney >= _speedUp * 50)
+        if (GM._totalMoney >= _speedUp * 50 && _speedUp < 30)
         {
             GM.TotalMoney(-_speedUp * 50);
             _speedUp++;
@@ -65,7 +93,7 @@ public class PowerUp : SingletonMonovihair<PowerUp>
     /// <summary>一度に出す機体を増やす関数</summary>
     public void AirNum()
     {
-        if (GM._totalMoney >= _airnum * 5000)
+        if (GM._totalMoney >= _airnum * 5000 && _airnum < 8)
         {
             GM.TotalMoney(-_airnum * 5000);
             _airnum++;
